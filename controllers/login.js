d0757bd82e685ae2d3cd3c326e097c8532a0c24a
andersonmendesdev <37872453+andersonmendesdev@users.noreply.github.com>
2018-08-06 15:49:50 -0400

    ok
    ok

--- controllers/login.js
+++ controllers/login.js
@@ -1,4 +1,27 @@
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+//generator bcrypt hash and salt
+const hashpass = (password) =>{
+    return new Promise((resolve, reject) =>{
+        bcrypt.genSalt((err, salt) =>{
+            if(err){
+                console.log(err)
+            }else{
+                bcrypt.hash(password, salt,(err, hash)=>{
+                    if(err){
+                        reject(err)
+                    }else{
+                        resolve(hash)
+                    }
+                })
+            }
+        })
+    })
+}
+
+
+
 const login = (req, res) => {
     if(req.session.user){
         return res.redirect('/')
@@ -11,14 +34,19 @@ const FormUser = (req, res) => {
 }
 
 const createUser =  async (connection, req, res) => {
+    req.body.password = await hashpass(req.body.password)
     await User.createUser(connection, req.body)
     res.redirect('/login')
 }
+//bcrypt hash
 const authenticateUser = async (connection,  req, res) => {
     
     const user = await User.findUser(connection, req.body.username)
     if(!user){
         return res.render('login/login',{error: true})
+    }
+    if(!await bcrypt.compare(req.body.password, user.password)){
+        return res.render('login/login', {error: true})
     }
     user.password = undefined
     req.session.user = user
